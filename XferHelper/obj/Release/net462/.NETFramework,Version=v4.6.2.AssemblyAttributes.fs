e3b63fb3605ff6c4c865c5569deca7a7f9c7ce3a
Brad Martin <bradmartin333@gmail.com>
2021-02-28 07:29:49 -0500

    .gitignore is now working

--- XferHelper/obj/Release/net462/.NETFramework,Version=v4.6.2.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.6.2", FrameworkDisplayName=".NET Framework 4.6.2")>]
-                do ()
