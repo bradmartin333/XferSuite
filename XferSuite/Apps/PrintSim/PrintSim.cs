7be48d4cd8d90a2dd8dd1b322ac8eaf2857cd15e
BradDesktop <brad.martin@xdisplay.com>
2021-03-04 11:54:47 -0500

    build source and target as renderobjects

--- XferSuite/Apps/PrintSim/PrintSim.cs
+++ XferSuite/Apps/PrintSim/PrintSim.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static XferSuite.Parameters;
+using static XferSuite.RenderObject;
 
 namespace XferSuite
 {
@@ -27,19 +28,47 @@ namespace XferSuite
             }
         }
 
+        private float _DeviceSizeX = 40F;
+        [
+            Category("User Parameters"),
+            Description("Width of device in microns")
+        ]
+        public float DeviceSizeX
+        {
+            get => _DeviceSizeX;
+            set
+            {
+                _DeviceSizeX = value;
+            }
+        }
+
+        private float _DeviceSizeY = 40F;
+        [
+            Category("User Parameters"),
+            Description("Height of device in microns")
+        ]
+        public float DeviceSizeY
+        {
+            get => _DeviceSizeY;
+            set
+            {
+                _DeviceSizeY = value;
+            }
+        }
+
         public PrintSim(string path)
         {
             InitializeComponent();
             LoadRecipe(path);
+            CreateSourceFeatures();
+            CreateTargetFeatures();
             MakePlot();
         }
 
-        //private List<RenderObject> _StampPosts = new List<RenderObject>();
-        //private List<RenderObject> _Wafers = new List<RenderObject>();
-        //private List<RenderObject> _Devices = new List<RenderObject>();
-        //private List<RenderObject> _Sites = new List<RenderObject>();
-        //private List<Vector2> _SourceBuilder = new List<Vector2>();
-        //private List<Vector2> _TargetBuilder = new List<Vector2>();
+        private List<RenderObject> _Devices = new List<RenderObject>();
+        private List<RenderObject> _Sites = new List<RenderObject>();
+        private List<PointF> _SourceBuilder = new List<PointF>();
+        private List<PointF> _TargetBuilder = new List<PointF>();
 
         private void btnOpenMap_Click(object sender, EventArgs e)
         {
@@ -68,87 +97,87 @@ namespace XferSuite
 
         private void CreateSourceFeatures()
         {
-            //for (int j = 0; j < SourceChiplets.Y; j++)
-            //{
-            //    for (int i = (int)SourceChiplets.X - 1; i >= 0; i--)
-            //    {
-            //        _SourceBuilder.Add(new Vector2(SourceOrigin.X + i * SourceChipletPitch.X, SourceOrigin.Y + j * SourceChipletPitch.Y));
-            //    }
-            //}
+            for (int j = 0; j < SourceChiplets.Y; j++)
+            {
+                for (int i = (int)SourceChiplets.X - 1; i >= 0; i--)
+                {
+                    _SourceBuilder.Add(new PointF(SourceOrigin.X + i * SourceChipletPitch.X, SourceOrigin.Y + j * SourceChipletPitch.Y));
+                }
+            }
 
-            //int rr = 0;
-            //for (int n = 0; n < SourceRegions.Y; n++)
-            //{
-            //    int rc = 0;
-            //    for (int m = (int)SourceRegions.X - 1; m >= 0; m--)
-            //    {
-            //        int r = 0;
-            //        for (int l = 0; l < SourceClusters.Y; l++)
-            //        {
-            //            int c = 0;
-            //            for (int k = (int)SourceClusters.X - 1; k >= 0; k--)
-            //            {
-            //                int idx = 0;
-            //                foreach (Vector2 pos in _SourceBuilder)
-            //                {
-            //                    RenderObject renderObject = new RenderObject(ObjectFactory.CreateRect(pos.X + k * SourceClusterPitch.X + m * SourceRegionPitch.X, pos.Y + l * SourceClusterPitch.Y + n * SourceRegionPitch.Y, DeviceSize.X, DeviceSize.Y, Color4.DarkBlue));
-            //                    renderObject.RR = rr + 1;
-            //                    renderObject.RC = rc + 1;
-            //                    renderObject.R = r + 1;
-            //                    renderObject.C = c + 1;
-            //                    renderObject.IDX = idx + 1;
-            //                    _Devices.Add(renderObject);
-            //                    idx++;
-            //                }
-            //                c++;
-            //            }
-            //            r++;
-            //        }
-            //        rc++;
-            //    }
-            //    rr++;
-            //}
+            int rr = 0;
+            for (int n = 0; n < SourceRegions.Y; n++)
+            {
+                int rc = 0;
+                for (int m = (int)SourceRegions.X - 1; m >= 0; m--)
+                {
+                    int r = 0;
+                    for (int l = 0; l < SourceClusters.Y; l++)
+                    {
+                        int c = 0;
+                        for (int k = (int)SourceClusters.X - 1; k >= 0; k--)
+                        {
+                            int idx = 0;
+                            foreach (PointF pos in _SourceBuilder)
+                            {
+                                RenderObject renderObject = new RenderObject(pos.X + k * SourceClusterPitch.X + m * SourceRegionPitch.X, pos.Y + l * SourceClusterPitch.Y + n * SourceRegionPitch.Y, _DeviceSizeX, _DeviceSizeY, Color.DarkBlue);
+                                renderObject.RR = rr + 1;
+                                renderObject.RC = rc + 1;
+                                renderObject.R = r + 1;
+                                renderObject.C = c + 1;
+                                renderObject.IDX = idx + 1;
+                                _Devices.Add(renderObject);
+                                idx++;
+                            }
+                            c++;
+                        }
+                        r++;
+                    }
+                    rc++;
+                }
+                rr++;
+            }
         }
 
         private void CreateTargetFeatures()
         {
-            //for (int j = 0; j < StampPosts.Y; j++)
-            //{
-            //    for (int i = (int)StampPosts.X - 1; i >= 0; i--)
-            //    {
-            //        _TargetBuilder.Add(new Vector2(TargetOrigin.X + i * StampPostPitch.X, TargetOrigin.Y + j * StampPostPitch.Y));
-            //    }
-            //}
+            for (int j = 0; j < StampPosts.Y; j++)
+            {
+                for (int i = (int)StampPosts.X - 1; i >= 0; i--)
+                {
+                    _TargetBuilder.Add(new PointF(TargetOrigin.X + i * StampPostPitch.X, TargetOrigin.Y + j * StampPostPitch.Y));
+                }
+            }
 
-            //int rr = 0;
-            //for (int n = 0; n < TargetClusters.Y; n++)
-            //{
-            //    int rc = 0;
-            //    for (int m = (int)TargetClusters.X - 1; m >= 0; m--)
-            //    {
-            //        int r = 0;
-            //        for (int l = 0; l < TargetPrints.Y; l++)
-            //        {
-            //            int c = 0;
-            //            for (int k = (int)TargetPrints.X - 1; k >= 0; k--)
-            //            {
-            //                foreach (Vector2 pos in _TargetBuilder)
-            //                {
-            //                    RenderObject renderObject = new RenderObject(ObjectFactory.CreateRect(pos.X + k * TargetPrintPitch.X + m * TargetClusterPitch.X, pos.Y + l * TargetPrintPitch.Y + n * TargetClusterPitch.Y, DeviceSize.X, DeviceSize.Y, Color4.DarkGreen));
-            //                    renderObject.RR = rr + 1;
-            //                    renderObject.RC = rc + 1;
-            //                    renderObject.R = r + 1;
-            //                    renderObject.C = c + 1;
-            //                    _Sites.Add(renderObject);
-            //                }
-            //                c++;
-            //            }
-            //            r++;
-            //        }
-            //        rc++;
-            //    }
-            //    rr++;
-            //}
+            int rr = 0;
+            for (int n = 0; n < TargetClusters.Y; n++)
+            {
+                int rc = 0;
+                for (int m = (int)TargetClusters.X - 1; m >= 0; m--)
+                {
+                    int r = 0;
+                    for (int l = 0; l < TargetPrints.Y; l++)
+                    {
+                        int c = 0;
+                        for (int k = (int)TargetPrints.X - 1; k >= 0; k--)
+                        {
+                            foreach (PointF pos in _TargetBuilder)
+                            {
+                                RenderObject renderObject = new RenderObject(pos.X + k * TargetPrintPitch.X + m * TargetClusterPitch.X, pos.Y + l * TargetPrintPitch.Y + n * TargetClusterPitch.Y, _DeviceSizeX, _DeviceSizeY, Color.DarkGreen);
+                                renderObject.RR = rr + 1;
+                                renderObject.RC = rc + 1;
+                                renderObject.R = r + 1;
+                                renderObject.C = c + 1;
+                                _Sites.Add(renderObject);
+                            }
+                            c++;
+                        }
+                        r++;
+                    }
+                    rc++;
+                }
+                rr++;
+            }
         }
     }
 }
---
+++ XferSuite/Apps/PrintSim/RenderObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace XferSuite
+{
+    public class RenderObject
+    {
+        public bool selected;
+        public float RR, RC, R, C, IDX;
+
+        public RenderObject(float x, float y, float width, float height, Color color)
+        {
+
+        }
+    }
+}
