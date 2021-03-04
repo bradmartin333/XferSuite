5dba565b074c74460e8a137916052a764ac0ecd9
BradDesktop <brad.martin@xdisplay.com>
2021-03-04 10:17:25 -0500

    add print list

--- XferSuite/Fingerprinting.Designer.cs
+++ XferSuite/Fingerprinting.Designer.cs
@@ -30,20 +30,68 @@ namespace XferSuite
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fingerprinting));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.PrintList = new System.Windows.Forms.ListBox();
+            this.plot = new OxyPlot.WindowsForms.PlotView();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.PrintList, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.plot, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(543, 424);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // PrintList
+            // 
+            this.PrintList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PrintList.FormattingEnabled = true;
+            this.PrintList.Location = new System.Drawing.Point(3, 3);
+            this.PrintList.Name = "PrintList";
+            this.PrintList.Size = new System.Drawing.Size(102, 418);
+            this.PrintList.TabIndex = 0;
+            this.PrintList.SelectedIndexChanged += new System.EventHandler(this.PrintList_SelectedIndexChanged);
+            // 
+            // plot
+            // 
+            this.plot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plot.Location = new System.Drawing.Point(111, 3);
+            this.plot.Name = "plot";
+            this.plot.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plot.Size = new System.Drawing.Size(429, 418);
+            this.plot.TabIndex = 1;
+            this.plot.Text = "plotView1";
+            this.plot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
             // 
             // Fingerprinting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(543, 424);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Fingerprinting";
             this.Text = "Fingerprinting";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox PrintList;
+        private OxyPlot.WindowsForms.PlotView plot;
     }
 }
--- XferSuite/Fingerprinting.cs
+++ XferSuite/Fingerprinting.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +20,30 @@ namespace XferSuite
         public Fingerprinting(Metro.Position[] data)
         {
             InitializeComponent();
+            _raw = data;
+
+            string[] indices = Metro.prints(_raw);
+            int printIdx = 1;
+            int posIdx = 0;
+            foreach (string index in indices)
+            {
+                if (!_prints.Contains(index))
+                {
+                    _prints.Add(index);
+                    PrintList.Items.Add(index);
+                    printIdx += 1;
+                }
+                _raw[posIdx].PrintNum = printIdx;
+                posIdx += 1;
+            }
+        }
+
+        private Metro.Position[] _raw;
+        List<string> _prints = new List<string>();
+
+        private void PrintList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            plot.BackColor = Color.Green;
         }
     }
 }
