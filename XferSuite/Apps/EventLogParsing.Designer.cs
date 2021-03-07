a0bccf375afe7886c3a04cd2f7e5d5b85697fb5b
BradDesktop <brad.martin@xdisplay.com>
2021-03-07 14:59:40 -0500

    turns out RTBs are really slow

--- XferSuite/Apps/EventLogParsing.Designer.cs
+++ XferSuite/Apps/EventLogParsing.Designer.cs
@@ -30,34 +30,85 @@ namespace XferSuite
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EventLogParsing));
-            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.rtbPrints = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtbMain = new System.Windows.Forms.RichTextBox();
+            this.lblDateRange = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // richTextBox
+            // rtbPrints
             // 
-            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox.Location = new System.Drawing.Point(0, 0);
-            this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(664, 165);
-            this.richTextBox.TabIndex = 0;
-            this.richTextBox.Text = "Currently only reports the datetime of prints.\nOpen to suggestions!\n\n";
+            this.rtbPrints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbPrints.Location = new System.Drawing.Point(2, 355);
+            this.rtbPrints.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbPrints.Name = "rtbPrints";
+            this.rtbPrints.Size = new System.Drawing.Size(494, 139);
+            this.rtbPrints.TabIndex = 0;
+            this.rtbPrints.Text = "Print List:\n";
+            this.rtbPrints.WordWrap = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.rtbPrints, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.rtbMain, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblDateRange, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(498, 496);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // rtbMain
+            // 
+            this.rtbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbMain.Location = new System.Drawing.Point(3, 23);
+            this.rtbMain.Name = "rtbMain";
+            this.rtbMain.Size = new System.Drawing.Size(492, 327);
+            this.rtbMain.TabIndex = 1;
+            this.rtbMain.Text = "";
+            this.rtbMain.WordWrap = false;
+            // 
+            // lblDateRange
+            // 
+            this.lblDateRange.AutoSize = true;
+            this.lblDateRange.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblDateRange.Location = new System.Drawing.Point(3, 0);
+            this.lblDateRange.Name = "lblDateRange";
+            this.lblDateRange.Size = new System.Drawing.Size(492, 20);
+            this.lblDateRange.TabIndex = 2;
+            this.lblDateRange.Text = "Start Date - End Date";
+            this.lblDateRange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // EventLogParsing
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 165);
-            this.Controls.Add(this.richTextBox);
+            this.ClientSize = new System.Drawing.Size(498, 496);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "EventLogParsing";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Event Log Parsing";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        public System.Windows.Forms.RichTextBox richTextBox;
+        public System.Windows.Forms.RichTextBox rtbPrints;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox rtbMain;
+        private System.Windows.Forms.Label lblDateRange;
     }
 }
--- XferSuite/Apps/EventLogParsing.cs
+++ XferSuite/Apps/EventLogParsing.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using XferHelper;
 
@@ -30,7 +23,7 @@ namespace XferSuite
             Parser.Event[] prints = Parser.main(_lines);
             foreach (Parser.Event p in prints)
             {
-                richTextBox.Text += p.Time + Environment.NewLine;
+                rtbPrints.Text += p.Time + Environment.NewLine;
             }
         }
     }
