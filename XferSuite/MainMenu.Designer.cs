007de524a33050ccabb1769f3ca91a54b30bd3d1
BradDesktop <brad.martin@xdisplay.com>
2021-09-08 07:45:03 -0400

    fix xyzplot bugs and update version window

--- XferSuite/MainMenu.Designer.cs
+++ XferSuite/MainMenu.Designer.cs
@@ -240,7 +240,7 @@ namespace XferSuite
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "XferSuite v1.15";
+            this.Text = "XferSuite v1.16";
             this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
