0b47af2fa4ce1411bd39638b378ecd0286a34411
BradDesktop <brad.martin@xdisplay.com>
2021-10-20 12:17:46 -0400

    update version

--- XferSuite/MainMenu.cs
+++ XferSuite/MainMenu.cs
@@ -11,7 +11,7 @@ namespace XferSuite
     public partial class MainMenu : Form
     {
         public static int MajorVersion = 2;
-        public static int MinorVerson = 3;
+        public static int MinorVerson = 4;
 
         public MainMenu()
         {
