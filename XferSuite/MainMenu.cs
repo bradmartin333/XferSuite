888981ae3ed55323e5657dd9c2015ae84d1c3fc5
BradDesktop <brad.martin@xdisplay.com>
2022-01-04 21:41:03 -0500

    v2.13 installer

--- XferSuite/MainMenu.cs
+++ XferSuite/MainMenu.cs
@@ -11,7 +11,7 @@ namespace XferSuite
     public partial class MainMenu : Form
     {
         public static int MajorVersion = 2;
-        public static int MinorVerson = 12;
+        public static int MinorVerson = 13;
 
         private static Settings _Settings = new Settings();
 
