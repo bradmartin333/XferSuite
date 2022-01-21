eac01bf0765acec0656306c3807cf3bef9bb4590
Brad Martin <bradmartin333@gmail.com>
2022-01-21 10:01:49 -0500

    v2.17 installer

--- XferSuite/MainMenu.cs
+++ XferSuite/MainMenu.cs
@@ -11,7 +11,7 @@ namespace XferSuite
     public partial class MainMenu : Form
     {
         public static int MajorVersion = 2;
-        public static int MinorVerson = 16;
+        public static int MinorVerson = 17;
 
         private static Settings _Settings = new Settings();
 
