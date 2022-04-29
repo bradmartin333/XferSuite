0f73ff449924c5cdba66330e8ee61870d0535696
BradDesktop <brad.martin@xdisplay.com>
2022-04-29 07:53:52 -0400

    create pass fail form

---
+++ XferSuite/Apps/SEYR/PassFailUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XferSuite.Apps.SEYR
+{
+    public partial class PassFailUtility : Form
+    {
+        public PassFailUtility()
+        {
+            InitializeComponent();
+        }
+    }
+}
