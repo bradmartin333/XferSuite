e7d31407e28c962151649fb3d5e19539683fe1c0
BradDesktop <brad.martin@xdisplay.com>
2022-05-27 11:06:12 -0400

    add if function

--- XferSuite/AdvancedTools/DataFilter.cs
+++ XferSuite/AdvancedTools/DataFilter.cs
@@ -15,7 +15,7 @@ namespace XferSuite.AdvancedTools
             public List<string> Args;
         }
 
-        enum Commands { COLS, COUNT, COUNTEX, DELETE, DELETEEX, REPLACE, REPLACEEX }
+        enum Commands { COLS, COUNT, COUNTEX, DELETE, DELETEEX, REPLACE, REPLACEEX, IF, IFEX, XIF, XIFEX }
         enum Delimeter { Tab, Space, Comma }
         private char Delim { 
             get
@@ -36,7 +36,9 @@ namespace XferSuite.AdvancedTools
         private FileInfo FileInfo = null;
         private string Header = null;
         private string LastEntry = string.Empty;
-        private List<string[]> Data = new List<string[]>();
+        private List<(int, string[])> Data = new List<(int, string[])>();
+        private List<(int, string[])> DataRemainder = new List<(int, string[])>();
+        private List<(int, string[])> DataSubset = new List<(int, string[])>();
         private List<bool> NumberColumn = new List<bool>();
 
         public DataFilter()
@@ -49,7 +51,8 @@ namespace XferSuite.AdvancedTools
         private void RTB_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter) ProcessRTB();
-            if (e.KeyCode == Keys.Up) PrintLastEntry();
+            else if (e.KeyCode == Keys.Up) PrintLastEntry();
+            else if (e.KeyCode == Keys.Down) RTB.Text = "";
         }
 
         private void PrintLastEntry()
@@ -72,7 +75,7 @@ namespace XferSuite.AdvancedTools
             }
 
             string[] commands = Enum.GetNames(typeof(Commands));
-            string msg = RTB.Text.Replace("\n", "");
+            string msg = RTB.Text.Split('\n')[0];
             string[] cols = msg.Split(' ');
             
             List<CommandProcessor> commandProcessors = new List<CommandProcessor>();
@@ -97,10 +100,14 @@ namespace XferSuite.AdvancedTools
             {
                 RTB.Text = string.Empty;
                 LastEntry = msg;
-            }    
+            }
 
             foreach (CommandProcessor cp in commandProcessors)
                 ExecuteCommand(cp);
+
+            DataRemainder.Clear();
+            DataSubset.Clear();
+            Data = Data.OrderBy(x => x.Item1).ToList();
         }
 
         private void ExecuteCommand(CommandProcessor cp)
@@ -131,6 +138,15 @@ namespace XferSuite.AdvancedTools
                     else
                         PrintReplace(cp.Args.ToArray(), cp.Command == Commands.COUNTEX);
                     break;
+                case Commands.IF:
+                case Commands.IFEX:
+                case Commands.XIF:
+                case Commands.XIFEX:
+                    if (cp.Args.Count > 3 || (cp.Args.Count == 3 && cp.Args[1] != "IN") || cp.Args.Count == 2)
+                        RTB.Text += "Invalid if command\n";
+                    else
+                        EvaluateIf(cp.Args.ToArray(), cp.Command);
+                    break;
                 default:
                     break;
             }
@@ -154,25 +170,28 @@ namespace XferSuite.AdvancedTools
 
         private void PrintCount(string[] vals, bool exact)
         {
+            List<(int, string[])> data = DataSubset.Count == 0 ? Data : DataSubset;
             try
             {
                 int num = 0;
-                if (vals.Length == 1)
+                if (vals.Length == 0)
+                    RTB.Text += $"Count = {data.Count}\n";
+                else if (vals.Length == 1)
                 {
-                    foreach (string[] row in Data)
-                        num += row.Where(x => exact ? x == vals[0] : x.Contains(vals[0])).Count();
+                    foreach ((int, string[]) row in data)
+                        num += row.Item2.Where(x => exact ? x == vals[0] : x.Contains(vals[0])).Count();
                     RTB.Text += $"{(exact ? "Exact c" : "C")}ount of {vals[0]} = {num}\n";
                 }
                 else
                 {
                     int colNum = int.Parse(vals[2]);
-                    foreach (string[] row in Data)
+                    foreach ((int, string[]) row in data)
                     {
-                        if (row.Length - 1 < colNum) continue;
+                        if (row.Item2.Length - 1 < colNum) continue;
                         if (exact)
-                            num += row[colNum] == vals[0] ? 1 : 0;
+                            num += row.Item2[colNum] == vals[0] ? 1 : 0;
                         else
-                            num += row[colNum].Contains(vals[0]) ? 1 : 0;
+                            num += row.Item2[colNum].Contains(vals[0]) ? 1 : 0;
                     }
                     RTB.Text += $"{(exact ? "Exact c" : "C")}ount of {vals[0]} in {vals[2]} = {num}\n";
                 }
@@ -185,34 +204,53 @@ namespace XferSuite.AdvancedTools
 
         private void PrintDelete(string[] vals, bool exact)
         {
+            List<(int, string[])> data = DataSubset.Count == 0 ? Data : DataSubset;
             try
             {
-                List<string[]> newData = new List<string[]>();
-                if (vals.Length == 1)
+                List<(int, string[])> newData = new List<(int, string[])>();
+                if (vals.Length == 0)
                 {
-                    foreach (string[] row in Data)
-                        if (!row.Where(x => exact ? x == vals[0] : x.Contains(vals[0])).Any()) newData.Add(row);
+                    RTB.Text += $"Deleted {data.Count} rows\n";
+                    data.Clear();
                 }
                 else
                 {
-                    int colNum = int.Parse(vals[2]);
-                    foreach (string[] row in Data)
+                    if (vals.Length == 1)
                     {
-                        bool match = false;
-                        if (row.Length - 1 < colNum)
-                        {
-                            newData.Add(row);
-                            continue;
-                        }
-                        if (exact)
-                            match = row[colNum] == vals[0];
-                        else
-                            match = row[colNum].Contains(vals[0]);
-                        if (!match) newData.Add(row);
+                        foreach ((int, string[]) row in data)
+                            if (!row.Item2.Where(x => exact ? x == vals[0] : x.Contains(vals[0])).Any()) newData.Add(row);
                     }
+                    else
+                    {
+                        int colNum = int.Parse(vals[2]);
+                        foreach ((int, string[]) row in data)
+                        {
+                            bool match = false;
+                            if (row.Item2.Length - 1 < colNum)
+                            {
+                                newData.Add(row);
+                                continue;
+                            }
+                            if (exact)
+                                match = row.Item2[colNum] == vals[0];
+                            else
+                                match = row.Item2[colNum].Contains(vals[0]);
+                            if (!match) newData.Add(row);
+                        }
+                    }
+                    RTB.Text += $"{(exact ? "Exact d" : "D")}eleted {Data.Count - newData.Count} rows\n";
                 }
-                RTB.Text += $"{(exact ? "Exact d" : "D")}eleted {Data.Count - newData.Count} rows\n";
-                Data = newData;
+
+                if (DataSubset.Count == 0)
+                {
+                    Data.Clear();
+                    Data.AddRange(DataRemainder);
+                    Data.AddRange(newData);
+                    DataRemainder.Clear();
+                    DataSubset.Clear();
+                }
+                else
+                    Data = newData;
             }
             catch (Exception ex)
             {
@@ -222,6 +260,7 @@ namespace XferSuite.AdvancedTools
 
         private void PrintReplace(string[] vals, bool exact)
         {
+            List<(int, string[])> data = DataSubset.Count == 0 ? Data : DataSubset;
             try
             {
                 string rep = string.Empty;
@@ -230,23 +269,23 @@ namespace XferSuite.AdvancedTools
                 int num = 0;
                 if (vals.Length == 2)
                 {
-                    foreach (string[] row in Data)
+                    foreach ((int, string[]) row in data)
                     {
-                        for (int i = 0; i < row.Length; i++)
+                        for (int i = 0; i < row.Item2.Length; i++)
                         {
                             if (exact)
                             {
-                                if (row[i] == vals[0])
+                                if (row.Item2[i] == vals[0])
                                 {
-                                    row[i] = rep;
+                                    row.Item2[i] = rep;
                                     num++;
                                 }
                             }
                             else
                             {
-                                if (row[i].Contains(vals[0]))
+                                if (row.Item2[i].Contains(vals[0]))
                                 {
-                                    row[i] = row[i].Replace(vals[0], rep);
+                                    row.Item2[i] = row.Item2[i].Replace(vals[0], rep);
                                     num++;
                                 }
                             }
@@ -257,27 +296,36 @@ namespace XferSuite.AdvancedTools
                 else
                 {
                     int colNum = int.Parse(vals[3]);
-                    foreach (string[] row in Data)
+                    foreach ((int, string[]) row in data)
                     {
-                        if (row.Length - 1 < colNum) continue;
+                        if (row.Item2.Length - 1 < colNum) continue;
                         if (exact)
                         {
-                            if (row[colNum] == vals[0])
+                            if (row.Item2[colNum] == vals[0])
                             {
-                                row[colNum] = rep;
+                                row.Item2[colNum] = rep;
                                 num++;
                             }
                         }
                         else
                         {
-                            if (row[colNum].Contains(vals[0]))
+                            if (row.Item2[colNum].Contains(vals[0]))
                             {
-                                row[colNum] = row[colNum].Replace(vals[0], rep);
+                                row.Item2[colNum] = row.Item2[colNum].Replace(vals[0], rep);
                                 num++;
                             }
                         }
                     }
                     RTB.Text += $"{(exact ? "Exact r" : "R")}eplaced {num} intances of {vals[0]} in {vals[3]}\n";
+                }
+
+                if (DataSubset.Count == 0)
+                {
+                    Data.Clear();
+                    Data.AddRange(DataRemainder);
+                    Data.AddRange(data);
+                    DataRemainder.Clear();
+                    DataSubset.Clear();
                 }
             }
             catch (Exception ex)
@@ -285,6 +333,75 @@ namespace XferSuite.AdvancedTools
                 RTB.Text += $"Invalid replace command {ex}\n";
             }
         }
+
+        private void EvaluateIf(string[] vals, Commands command)
+        {
+            List<(int, string[])> data = DataSubset.Count == 0 ? Data : DataSubset;
+            List<(int, string[])> thisRemainder = new List<(int, string[])>();
+            List<(int, string[])> thisSubset = new List<(int, string[])>();
+            if (vals.Length == 1)
+            {
+                foreach ((int, string[]) row in data)
+                {
+                    switch (command)
+                    {
+                        case Commands.IF:
+                        case Commands.XIF:
+                            if (row.Item2.Where(x => x.Contains(vals[0])).Count() > 0 && command == Commands.IF)
+                                thisSubset.Add(row);
+                            else if (row.Item2.Where(x => x.Contains(vals[0])).Count() == 0 && command == Commands.XIF)
+                                thisSubset.Add(row);
+                            else
+                                thisRemainder.Add(row);
+                            break;
+                        case Commands.IFEX:
+                        case Commands.XIFEX:
+                            if (row.Item2.Where(x => x == vals[0]).Count() > 0 && command == Commands.IFEX)
+                                thisSubset.Add(row);
+                            else if (row.Item2.Where(x => x == vals[0]).Count() == 0 && command == Commands.XIFEX)
+                                thisSubset.Add(row);
+                            else
+                                thisRemainder.Add(row);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                int colNum = int.Parse(vals[2]);
+                foreach ((int, string[]) row in data)
+                {
+                    switch (command)
+                    {
+                        case Commands.IF:
+                        case Commands.XIF:
+                            if (row.Item2[colNum].Contains(vals[0]) && command == Commands.IF)
+                                thisSubset.Add(row);
+                            else if (!row.Item2[colNum].Contains(vals[0]) && command == Commands.XIF)
+                                thisSubset.Add(row);
+                            else
+                                thisRemainder.Add(row);
+                            break;
+                        case Commands.IFEX:
+                        case Commands.XIFEX:
+                            if (row.Item2[colNum] == vals[0] && command == Commands.IFEX)
+                                thisSubset.Add(row);
+                            else if (row.Item2[colNum] != vals[0] && command == Commands.XIFEX)
+                                thisSubset.Add(row);
+                            else
+                                thisRemainder.Add(row);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            DataRemainder.AddRange(thisRemainder);
+            DataSubset = thisSubset;
+        }
+
 
         #endregion
 
@@ -314,7 +431,7 @@ namespace XferSuite.AdvancedTools
             if (FileInfo == null || !FileInfo.Exists) return;
 
             Header = null;
-            Data = new List<string[]>();
+            Data = new List<(int, string[])>();
             NumberColumn = new List<bool>();
 
             string[] lines = File.ReadAllLines(FileInfo.FullName);
@@ -331,7 +448,7 @@ namespace XferSuite.AdvancedTools
                 NumberColumn.Add(float.TryParse(col, out _));
 
             for (int i = idx; i < lines.Length; i++)
-                if (!string.IsNullOrEmpty(lines[i])) Data.Add(lines[i].Split(Delim));
+                if (!string.IsNullOrEmpty(lines[i])) Data.Add((i, lines[i].Split(Delim)));
 
             LblData.Text = $"{Data.Count} Rows Loaded";
         }
@@ -340,8 +457,8 @@ namespace XferSuite.AdvancedTools
         {
             StringBuilder sb = new StringBuilder();
             if (!string.IsNullOrEmpty(Header)) sb.AppendLine(Header);
-            foreach (string[] row in Data)
-                sb.AppendLine(string.Join(Delim.ToString(), row));
+            foreach ((int, string[]) row in Data)
+                sb.AppendLine(string.Join(Delim.ToString(), row.Item2));
             return sb.ToString();
         }
 
