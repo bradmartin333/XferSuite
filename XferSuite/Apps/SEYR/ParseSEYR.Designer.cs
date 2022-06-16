adf2ea607df281df27e15c663a7cbfed3d90c389
Brad <brad.martin@xdisplay.com>
2022-06-16 16:36:00 -0400

    change background of OLV and close #194

--- XferSuite/Apps/SEYR/ParseSEYR.Designer.cs
+++ XferSuite/Apps/SEYR/ParseSEYR.Designer.cs
@@ -39,6 +39,8 @@
             this.criteriaOlvPass = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.criteriaOlvName = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.criteriaOlvID = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.MenuStrip = new System.Windows.Forms.MenuStrip();
             this.ToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.SaveAsNewSEYRUPToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
@@ -46,11 +48,9 @@
             this.MakeCycleFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ResetCriteiraGroupingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ResetCriteriaPlotSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GroupFeaturesIntoCriteriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.PlotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.LoadingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.GroupFeaturesIntoCriteriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.TLP.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.FeatureOLV)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.CriteriaOLV)).BeginInit();
@@ -87,7 +87,7 @@
             this.FeatureOLV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.FeatureOLV.BackColor = System.Drawing.Color.Gainsboro;
+            this.FeatureOLV.BackColor = System.Drawing.Color.WhiteSmoke;
             this.FeatureOLV.CellEditUseWholeCell = false;
             this.FeatureOLV.CheckBoxes = true;
             this.FeatureOLV.CheckedAspectName = "Ignore";
@@ -183,6 +183,28 @@
             this.criteriaOlvID.Text = "ID";
             this.criteriaOlvID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Feature Manager";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(328, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Plotted Criteria";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // MenuStrip
             // 
             this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -244,6 +266,13 @@
             this.ResetCriteriaPlotSettingsToolStripMenuItem.Text = "Reset Criteria Plot Settings";
             this.ResetCriteriaPlotSettingsToolStripMenuItem.Click += new System.EventHandler(this.ResetCriteriaPlotSettingsToolStripMenuItem_Click);
             // 
+            // GroupFeaturesIntoCriteriaToolStripMenuItem
+            // 
+            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Name = "GroupFeaturesIntoCriteriaToolStripMenuItem";
+            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Size = new System.Drawing.Size(237, 22);
+            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Text = "Group Features Into Criteria (g)";
+            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Click += new System.EventHandler(this.GroupFeaturesIntoCriteriaToolStripMenuItem_Click);
+            // 
             // PlotToolStripMenuItem
             // 
             this.PlotToolStripMenuItem.AutoSize = false;
@@ -263,35 +292,6 @@
             this.LoadingToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
             this.LoadingToolStripMenuItem.Text = "Loading...";
             this.LoadingToolStripMenuItem.Visible = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(3, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(328, 20);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Feature Manager";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(3, 203);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(328, 20);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "Plotted Criteria";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // GroupFeaturesIntoCriteriaToolStripMenuItem
-            // 
-            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Name = "GroupFeaturesIntoCriteriaToolStripMenuItem";
-            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Size = new System.Drawing.Size(237, 22);
-            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Text = "Group Features Into Criteria (g)";
-            this.GroupFeaturesIntoCriteriaToolStripMenuItem.Click += new System.EventHandler(this.GroupFeaturesIntoCriteriaToolStripMenuItem_Click);
             // 
             // ParseSEYR
             // 
