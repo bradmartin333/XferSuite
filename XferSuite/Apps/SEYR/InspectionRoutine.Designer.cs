ef0438487fc68b3bc7382a95b1bfd8a2ed523103
BradDesktop <brad.martin@xdisplay.com>
2022-07-15 10:09:49 -0400

    rename inspection routine button to close #241

--- XferSuite/Apps/SEYR/InspectionRoutine.Designer.cs
+++ XferSuite/Apps/SEYR/InspectionRoutine.Designer.cs
@@ -32,30 +32,32 @@
             this.TLP = new System.Windows.Forms.TableLayoutPanel();
             this.ListBoxNames = new System.Windows.Forms.ListBox();
             this.TextBoxName = new System.Windows.Forms.TextBox();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.BtnPrevious = new System.Windows.Forms.Button();
             this.BtnNext = new System.Windows.Forms.Button();
             this.BtnDone = new System.Windows.Forms.Button();
             this.PBX = new System.Windows.Forms.PictureBox();
             this.TLP.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PBX)).BeginInit();
             this.SuspendLayout();
             // 
             // TLP
             // 
-            this.TLP.ColumnCount = 2;
-            this.TLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
-            this.TLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.TLP.ColumnCount = 3;
+            this.TLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 64F));
+            this.TLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.TLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18F));
             this.TLP.Controls.Add(this.ListBoxNames, 1, 0);
+            this.TLP.Controls.Add(this.BtnNext, 2, 2);
             this.TLP.Controls.Add(this.TextBoxName, 1, 1);
-            this.TLP.Controls.Add(this.flowLayoutPanel1, 1, 2);
+            this.TLP.Controls.Add(this.BtnDone, 1, 3);
             this.TLP.Controls.Add(this.PBX, 0, 0);
+            this.TLP.Controls.Add(this.BtnPrevious, 1, 2);
             this.TLP.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TLP.Location = new System.Drawing.Point(0, 0);
             this.TLP.Name = "TLP";
-            this.TLP.RowCount = 3;
+            this.TLP.RowCount = 4;
             this.TLP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TLP.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.TLP.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.TLP.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.TLP.Size = new System.Drawing.Size(647, 440);
@@ -63,77 +65,68 @@
             // 
             // ListBoxNames
             // 
+            this.TLP.SetColumnSpan(this.ListBoxNames, 2);
             this.ListBoxNames.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ListBoxNames.FormattingEnabled = true;
             this.ListBoxNames.Items.AddRange(new object[] {
             "null"});
-            this.ListBoxNames.Location = new System.Drawing.Point(423, 3);
+            this.ListBoxNames.Location = new System.Drawing.Point(417, 3);
             this.ListBoxNames.Name = "ListBoxNames";
-            this.ListBoxNames.Size = new System.Drawing.Size(221, 373);
+            this.ListBoxNames.Size = new System.Drawing.Size(227, 350);
             this.ListBoxNames.TabIndex = 3;
             this.ListBoxNames.SelectedIndexChanged += new System.EventHandler(this.ListBoxNames_SelectedIndexChanged);
             // 
             // TextBoxName
             // 
+            this.TLP.SetColumnSpan(this.TextBoxName, 2);
             this.TextBoxName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextBoxName.Location = new System.Drawing.Point(423, 382);
+            this.TextBoxName.Location = new System.Drawing.Point(417, 359);
             this.TextBoxName.Name = "TextBoxName";
-            this.TextBoxName.Size = new System.Drawing.Size(221, 20);
+            this.TextBoxName.Size = new System.Drawing.Size(227, 20);
             this.TextBoxName.TabIndex = 4;
             this.TextBoxName.Text = "null";
             this.TextBoxName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.TextBoxName.TextChanged += new System.EventHandler(this.TextBoxName_TextChanged);
             // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.flowLayoutPanel1.AutoSize = true;
-            this.flowLayoutPanel1.Controls.Add(this.BtnPrevious);
-            this.flowLayoutPanel1.Controls.Add(this.BtnNext);
-            this.flowLayoutPanel1.Controls.Add(this.BtnDone);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(437, 408);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(193, 29);
-            this.flowLayoutPanel1.TabIndex = 5;
-            // 
             // BtnPrevious
             // 
-            this.BtnPrevious.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.BtnPrevious.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnPrevious.BackgroundImage")));
             this.BtnPrevious.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnPrevious.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BtnPrevious.FlatAppearance.BorderSize = 0;
             this.BtnPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnPrevious.Location = new System.Drawing.Point(3, 3);
+            this.BtnPrevious.Location = new System.Drawing.Point(417, 385);
             this.BtnPrevious.Name = "BtnPrevious";
-            this.BtnPrevious.Size = new System.Drawing.Size(50, 23);
+            this.BtnPrevious.Size = new System.Drawing.Size(110, 23);
             this.BtnPrevious.TabIndex = 0;
             this.BtnPrevious.UseVisualStyleBackColor = true;
             this.BtnPrevious.Click += new System.EventHandler(this.BtnPrevious_Click);
             // 
             // BtnNext
             // 
-            this.BtnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.BtnNext.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnNext.BackgroundImage")));
             this.BtnNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnNext.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BtnNext.FlatAppearance.BorderSize = 0;
             this.BtnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnNext.Location = new System.Drawing.Point(59, 3);
+            this.BtnNext.Location = new System.Drawing.Point(533, 385);
             this.BtnNext.Name = "BtnNext";
-            this.BtnNext.Size = new System.Drawing.Size(50, 23);
+            this.BtnNext.Size = new System.Drawing.Size(111, 23);
             this.BtnNext.TabIndex = 1;
             this.BtnNext.UseVisualStyleBackColor = true;
             this.BtnNext.Click += new System.EventHandler(this.BtnNext_Click);
             // 
             // BtnDone
             // 
-            this.BtnDone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.BtnDone.BackColor = System.Drawing.Color.White;
+            this.TLP.SetColumnSpan(this.BtnDone, 2);
+            this.BtnDone.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BtnDone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnDone.Location = new System.Drawing.Point(115, 3);
+            this.BtnDone.Location = new System.Drawing.Point(417, 414);
             this.BtnDone.Name = "BtnDone";
-            this.BtnDone.Size = new System.Drawing.Size(75, 23);
+            this.BtnDone.Size = new System.Drawing.Size(227, 23);
             this.BtnDone.TabIndex = 2;
-            this.BtnDone.Text = "Done";
+            this.BtnDone.Text = "Close and Copy";
             this.BtnDone.UseVisualStyleBackColor = false;
             this.BtnDone.Click += new System.EventHandler(this.BtnDone_Click);
             // 
@@ -143,8 +136,8 @@
             this.PBX.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PBX.Location = new System.Drawing.Point(3, 3);
             this.PBX.Name = "PBX";
-            this.TLP.SetRowSpan(this.PBX, 3);
-            this.PBX.Size = new System.Drawing.Size(414, 434);
+            this.TLP.SetRowSpan(this.PBX, 4);
+            this.PBX.Size = new System.Drawing.Size(408, 434);
             this.PBX.TabIndex = 6;
             this.PBX.TabStop = false;
             // 
@@ -159,7 +152,6 @@
             this.Text = "Inspection Routine";
             this.TLP.ResumeLayout(false);
             this.TLP.PerformLayout();
-            this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.PBX)).EndInit();
             this.ResumeLayout(false);
 
@@ -170,7 +162,6 @@
         private System.Windows.Forms.TableLayoutPanel TLP;
         private System.Windows.Forms.ListBox ListBoxNames;
         private System.Windows.Forms.TextBox TextBoxName;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button BtnPrevious;
         private System.Windows.Forms.Button BtnNext;
         private System.Windows.Forms.Button BtnDone;
